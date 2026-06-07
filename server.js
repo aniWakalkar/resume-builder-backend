2870e6658c8e790997b22810ac6a8ff1abf2c3ae
master <aniketwakalkar23@gmail.com>
2026-06-07 23:36:36 +0530

    google auth working properly

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.LOCAL_CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.DEVELOPMENT_CLIENT_URL || 'http://localhost:5173',
   credentials: true
 }));
 app.use(express.json());
